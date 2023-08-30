37376ab4cc26a9beb98f97da8bc5fd9c2b294327
Adrian Cojocariu <aco@umbraco.dk>
2023-08-30 16:51:53 +0300

    Return record value in a HubSpot specific format

---
+++ src/Umbraco.Forms.Integrations.Crm.Hubspot/Extensions/RecordFieldExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Forms.Core.Data.Helpers;
+using Umbraco.Forms.Core.Persistence.Dtos;
+
+namespace Umbraco.Forms.Integrations.Crm.Hubspot.Extensions
+{
+    public static class RecordFieldExtensions
+    {
+        public static string ValuesAsHubspotString(this RecordField recordField, bool escaped = true)
+        {
+            if (!recordField.HasValue())
+            {
+                return string.Empty;
+            }
+
+            return escaped ? string.Join(";", 
+                recordField.Values
+                    .ConvertAll((object input) => JsonHelper.EscapeStringValue(input.ToString())).ToArray()) : string.Join(";", recordField.Values.ToArray());
+        }
+    }
+}
