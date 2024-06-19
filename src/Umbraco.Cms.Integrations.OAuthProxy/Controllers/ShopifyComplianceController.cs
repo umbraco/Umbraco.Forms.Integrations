3b89c69b64ae8524008f6c5d50803a2e4a0fd080
Adrian Cojocariu <aco@umbraco.dk>
2024-06-19 14:24:33 +0300

    Add Shopify compliance endpoints

---
+++ src/Umbraco.Cms.Integrations.OAuthProxy/Controllers/ShopifyComplianceController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Umbraco.Cms.Integrations.OAuthProxy.Controllers
+{
+    [ApiController]
+    public class ShopifyComplianceController : Controller
+    {
+        /// <summary>
+        /// Handles customer data requests from Shopify
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("/shopify-compliance/v1/customer/data-request")]
+        public IActionResult CustomerDataRequest() => Ok();
+
+        /// <summary>
+        /// Handles customer data erasure requests from Shopify
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("/shopify-compliance/v1/customer/data-redact")]
+        public IActionResult CustomerDataRedact() => Ok();
+
+        /// <summary>
+        /// Handles shop data erasure requests from Shopify
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("/shopify-compliance/v1/shop/data-redact")]
+        public IActionResult ShopDataRedact() => Ok();
+    }
+}
