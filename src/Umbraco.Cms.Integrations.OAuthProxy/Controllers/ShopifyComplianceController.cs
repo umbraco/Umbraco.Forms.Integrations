1cf91618b20b3af4f0c3450c79f5682920316585
Adrian Cojocariu <aco@umbraco.dk>
2024-07-03 15:00:49 +0300

    Fix formatting

--- src/Umbraco.Cms.Integrations.OAuthProxy/Controllers/ShopifyComplianceController.cs
+++ src/Umbraco.Cms.Integrations.OAuthProxy/Controllers/ShopifyComplianceController.cs
@@ -3,8 +3,8 @@ using Umbraco.Cms.Integrations.OAuthProxy.Filters;
 
 namespace Umbraco.Cms.Integrations.OAuthProxy.Controllers
 {
-    [ApiController]
-    public class ShopifyComplianceController : Controller
+	[ApiController]
+	public class ShopifyComplianceController : Controller
     {
         /// <summary>
         /// Handles customer data requests from Shopify
@@ -12,8 +12,8 @@ namespace Umbraco.Cms.Integrations.OAuthProxy.Controllers
         /// <returns></returns>
         [HttpPost]
         [Route("/shopify-compliance/v1/customer/data-request")]
-        [SignatureValidation]
-        public IActionResult CustomerDataRequest() => Ok();
+		[SignatureValidation]
+		public IActionResult CustomerDataRequest() => Ok();
 
         /// <summary>
         /// Handles customer data erasure requests from Shopify
@@ -21,8 +21,8 @@ namespace Umbraco.Cms.Integrations.OAuthProxy.Controllers
         /// <returns></returns>
         [HttpPost]
         [Route("/shopify-compliance/v1/customer/data-redact")]
-        [SignatureValidation]
-        public IActionResult CustomerDataRedact() => Ok();
+		[SignatureValidation]
+		public IActionResult CustomerDataRedact() => Ok();
 
         /// <summary>
         /// Handles shop data erasure requests from Shopify
@@ -30,7 +30,7 @@ namespace Umbraco.Cms.Integrations.OAuthProxy.Controllers
         /// <returns></returns>
         [HttpPost]
         [Route("/shopify-compliance/v1/shop/data-redact")]
-        [SignatureValidation]
-        public IActionResult ShopDataRedact() => Ok();
+		[SignatureValidation]
+		public IActionResult ShopDataRedact() => Ok();
     }
 }
