3ef4503686b08d92bb3078533677198e736168af
Nguyen Doan Thanh <145089181+dtng95@users.noreply.github.com>
2024-09-12 14:48:17 +0700

    V14: Integrations (ActiveCampaign/Forms)
    - Add base management api

---
+++ src/Umbraco.Forms.Integrations.Crm.ActiveCampaign/Api/Management/Controllers/Accounts/AccountControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Forms.Integrations.Crm.ActiveCampaign.Api.Management.Controllers.Accounts
+{
+    public class AccountControllerBase : ActiveCampaingControllerBase
+    {
+    }
+}
---
+++ src/Umbraco.Forms.Integrations.Crm.ActiveCampaign/Api/Management/Controllers/ActiveCampaingControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Api.Common.Attributes;
+using Umbraco.Cms.Web.Common.Authorization;
+
+namespace Umbraco.Forms.Integrations.Crm.ActiveCampaign.Api.Management.Controllers
+{
+    [ApiController]
+    [Authorize(Policy = AuthorizationPolicies.BackOfficeAccess)]
+    [MapToApi(Constants.ManagementApi.ApiName)]
+    public class ActiveCampaingControllerBase : Controller
+    {
+    }
+}
