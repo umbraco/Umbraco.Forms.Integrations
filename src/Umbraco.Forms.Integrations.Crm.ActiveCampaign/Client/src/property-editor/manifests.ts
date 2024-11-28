9b776293d22611c83bdb14e830ccf0e6aae4c860
Nguyen Doan Thanh <145089181+dtng95@users.noreply.github.com>
2024-11-28 18:16:23 +0700

    Update manifests.ts
    - update manifest types of array

--- src/Umbraco.Forms.Integrations.Crm.ActiveCampaign/Client/src/property-editor/manifests.ts
+++ src/Umbraco.Forms.Integrations.Crm.ActiveCampaign/Client/src/property-editor/manifests.ts
@@ -2,7 +2,7 @@ import { manifest as accountManifest } from "./account/manifests";
 import { manifest as contactMappingManifest } from "./contact-mapping/manifests";
 import { manifest as customMappingManifest } from "./custom-mapping/manifests";
 
-export const manifests = [
+export const manifests: Array<UmbExtensionManifest> = [
     accountManifest,
     contactMappingManifest,
     customMappingManifest
