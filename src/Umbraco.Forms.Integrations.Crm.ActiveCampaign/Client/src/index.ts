4b654013d377e3f3853bdabeb08d95e95b5754de
Adrian Cojocariu <aco@umbraco.dk>
2025-07-01 17:18:34 +0300

    Update auth client config and versioning

--- src/Umbraco.Forms.Integrations.Crm.ActiveCampaign/Client/src/index.ts
+++ src/Umbraco.Forms.Integrations.Crm.ActiveCampaign/Client/src/index.ts
@@ -2,6 +2,7 @@
 import { UMB_AUTH_CONTEXT } from "@umbraco-cms/backoffice/auth";
 import { manifests as propertyEditorManifests } from "./property-editor/manifests";
 import { client } from "@umbraco-integrations/activecampaign/generated";
+import { umbHttpClient } from "@umbraco-cms/backoffice/http-client";
 import { manifests as localizationManifests } from "./lang/manifests.js";
 import { manifest as activecampaignContext } from "./context/manifest.js";
 
@@ -13,12 +14,8 @@ export const onInit: UmbEntryPointOnInit = (host, extensionRegistry) => {
   ]);
 
   host.consumeContext(UMB_AUTH_CONTEXT, async (auth) => {
-      const config = auth?.getOpenApiConfiguration();
+      if (!auth) return;
 
-      client.setConfig({
-          auth: config?.token ?? undefined,
-          baseUrl: config?.base ?? "",
-          credentials: config?.credentials ?? "same-origin",
-      });
+      client.setConfig(umbHttpClient.getConfig());
   });
 };
